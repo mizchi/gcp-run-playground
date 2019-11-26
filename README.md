53a03317eb8fa644a23c310a6db6a51cddadea5b
mizchi <kotaro.chikuba@plaid.co.jp>
2019-11-26 17:14:22 +0900

    Update

---
+++ README.md
@@ -0,0 +1,16 @@
+# GCP Cloudrun Playground
+
+- CloudRun
+- CloudBuild
+- BigTable
+
+## Setup
+
+- Enable cloudrun: https://codelabs.developers.google.com/codelabs/cloud-run-hello/
+- Setup IAM for cloudbuild: https://cloud.google.com/cloud-build/docs/deploying-builds/deploy-cloud-run
+- Configure your: `.env` (`cp .env.example .env`)
+- Configure your: `cloudbuild.yaml`
+
+## LICENSE
+
+MIT
---
+++ scripts/createTable.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv");
+const Bigtable = require("@google-cloud/bigtable");
+
+dotenv.config();
+
+const { COLUMN_FAMILY_ID, INSTANCE_ID, TABLE_ID } = process.env;
+
+if (!INSTANCE_ID) {
+  throw new Error("Environment variables for INSTANCE_ID must be set!");
+}
+
+const bigtableClient = new Bigtable();
+
+(async () => {
+  try {
+    const instance = bigtableClient.instance(INSTANCE_ID);
+    const table = instance.table(TABLE_ID);
+    const [tableExists] = await table.exists();
+    if (!tableExists) {
+      const options = {
+        families: [
+          {
+            name: COLUMN_FAMILY_ID,
+            rule: {
+              versions: 1
+            }
+          }
+        ]
+      };
+      await table.create(options);
+      console.log("Table created");
+    } else {
+      console.log("Table already exists");
+    }
+  } catch (error) {
+    console.error("Something went wrong:", error);
+  }
+})();
